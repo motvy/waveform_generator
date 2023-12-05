d9d1d8d1b30a334b7fc78cc9c4c62e88bee91087
motvy <motvy33@gmail.com>
2023-12-05 19:09:17 +0300

    Added demo serial port

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,8 +1,9 @@
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QSize, QTimer
-from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QWidget
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QWidget, QMessageBox
 
 import datetime
+import serial
 
 from gui.control_panel import ControlPanel
 from gui.graph_canvas import GraphCanvas
@@ -53,6 +54,11 @@ class MainWindow(QWidget):
         self.initUI()
         self.initData()
 
+        self.serial_port = serial.Serial('COM1', 9600, timeout=1)
+        self.control_panel.setEnabled(False)
+        self.start_btn.setEnabled(False)
+        self.start_wait_signal()
+
     def initUI(self):
         self.control_panel = ControlPanel(self)
         self.graph_canvas = GraphCanvas(self)
@@ -64,20 +70,20 @@ class MainWindow(QWidget):
         self.start_btn.setStyleSheet('background: rgb(240, 240, 240); border: 0px;')
         self.start_btn.clicked.connect(self.start_generate)
 
-        self.stop_btn = QPushButton()
-        self.stop_btn = QPushButton()
-        self.stop_btn.setFixedSize(int(self.width*0.04), int(self.width*0.04))
-        self.stop_btn.setIcon(QIcon(config.stop_icon_path))
-        self.stop_btn.setIconSize(QSize(int(self.width*0.075), int(self.width*0.075)))
-        self.stop_btn.setStyleSheet('background: rgb(240, 240, 240); border: 0px;')
+        # self.stop_btn = QPushButton()
+        # self.stop_btn = QPushButton()
+        # self.stop_btn.setFixedSize(int(self.width*0.04), int(self.width*0.04))
+        # self.stop_btn.setIcon(QIcon(config.stop_icon_path))
+        # self.stop_btn.setIconSize(QSize(int(self.width*0.075), int(self.width*0.075)))
+        # self.stop_btn.setStyleSheet('background: rgb(240, 240, 240); border: 0px;')
 
-        self.timer_lb = QLabel(str(datetime.timedelta(seconds=0)))
+        self.status_lb = QLabel()
         
         button_layout = QHBoxLayout()
         button_layout.addWidget(self.start_btn)
-        button_layout.addWidget(self.stop_btn)
+        # button_layout.addWidget(self.stop_btn)
         button_layout.addStretch()
-        button_layout.addWidget(self.timer_lb)
+        button_layout.addWidget(self.status_lb)
         
         main_layout = QVBoxLayout()
         main_layout.addLayout(self.control_panel.layout)
@@ -87,34 +93,47 @@ class MainWindow(QWidget):
         self.setLayout(main_layout)
 
     def initData(self):
-        self.timer_count = 0
-        self.timer_flag = False
+        self.wait_flag = False
         timer = QTimer(self)
-        timer.timeout.connect(self.show_time)
+        timer.timeout.connect(self.check_uart)
         timer.start(1000)
 
     def start_generate(self):
-        self.start_timer()
-        self.stop_btn.clicked.connect(self.stop_generate)
         self.start_btn.clicked.disconnect()
         self.control_panel.setEnabled(False)
+        self.start_btn.setEnabled(False)
+
+        data = [0, 10, 20, 30, 40, 50, 60, 70]
+        data_arr = bytearray(data)
+        self.serial_port.write(data_arr)
+
+        line = self.serial_port.readline()
+        if line:
+            self.start_wait_signal()
+        else:
+            QMessageBox.critical(self, 'Функциональный генератор сигналов', 'МК не отвечает. Попробуйте снова.')
+            self.control_panel.setEnabled(True)
+            self.start_btn.setEnabled(True)
+            self.start_btn.clicked.connect(self.start_generate)
 
     def stop_generate(self):
         self.stop_timer()
         self.start_btn.clicked.connect(self.start_generate)
-        self.stop_btn.clicked.disconnect()
         self.control_panel.setEnabled(True)
 
-    def show_time(self):
-        if self.timer_flag:
-            self.timer_count+= 1
- 
-        self.timer_lb.setText(str(datetime.timedelta(seconds=self.timer_count)))
+    def check_uart(self):
+        if self.wait_flag:
+            line = self.serial_port.readline()
+            if line:
+                self.stop_timer()
 
-    def start_timer(self):
-        self.timer_flag = True
+    def start_wait_signal(self):
+        self.wait_flag = True
+        self.status_lb.setText("Для задания/редактирования сигнала воспользуйтесь кнопкой на МК")
  
     def stop_timer(self):
-        self.timer_flag = False
-        self.timer_count = 0
-        self.timer_lb.setText(str(datetime.timedelta(seconds=self.timer_count)))
+        self.wait_flag = False
+        self.status_lb.setText("")
+        self.start_btn.clicked.connect(self.start_generate)
+        self.control_panel.setEnabled(True)
+        self.start_btn.setEnabled(True)
